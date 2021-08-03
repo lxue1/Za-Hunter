b5a8cd920b37dd7b1101a511a1c8bb668a3f4740
Luke Xue <lukexue064@gmail.com>
2021-08-02 23:01:46 -0500

    Initial Commit

---
+++ Za Hunter/Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Za_HunterApp.swift
+//  Za Hunter
+//
+//  Created by Luke Xue on 8/2/21.
+//
+
+import SwiftUI
+
+@main
+struct Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
